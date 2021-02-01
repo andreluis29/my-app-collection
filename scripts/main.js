3d158a18fa437d81cd94d6f22800a163ed45fe22
andre <andre@localhost.localdomain>
2021-02-01 15:44:46 -0300

    Pequena correcao de bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,26 @@
 var counter = 1;
-if(!window.matchMedia("(min-width: 1024px)").matches) {
-    document.getElementById("button-dropdown").onclick = () => {   
-        if (counter == 2) {
-            document.getElementById("menu").style = "display: none";
-            counter = 1;
-        } else {
-            document.getElementById("menu").style = "display: block";
-            counter++;
+
+function menuChange(media) {
+    if (media.matches) {
+        document.getElementById("menu").style = "display: block"
+        document.getElementById("button-dropdown").style = "display: none"
+    } else {
+        document.getElementById("button-dropdown").style = "display: block"
+        document.getElementById("menu").style = "display: none"
+        document.getElementById("button-dropdown").onclick = function () {
+            console.log(counter)
+            if(counter > 1) {
+                counter = 1;
+                document.getElementById("menu").style = "display: block"
+            } else {
+                document.getElementById("menu").style = "display: none"
+                counter++
+            }
         }
-    }  
-}
+    }
+}  
+
+const media = window.matchMedia("(min-width: 1024px)")
+menuChange(media)
+media.addListener(menuChange)
 
