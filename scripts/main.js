11787d2ba004c91fe3d5d521b469085db92ee19c
andre <andre@localhost.localdomain>
2021-02-01 15:03:53 -0300

    Corrigido alguns detalhes de layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 var counter = 1;
-
-document.getElementById("button-dropdown").onclick = () => {   
-    if (counter == 2) {
-        document.getElementById("menu").style = "display: none";
-        counter = 1;
-    } else {
-        document.getElementById("menu").style = "display: block";
-        counter++;
-    }
+if(!window.matchMedia("(min-width: 1024px)").matches) {
+    document.getElementById("button-dropdown").onclick = () => {   
+        if (counter == 2) {
+            document.getElementById("menu").style = "display: none";
+            counter = 1;
+        } else {
+            document.getElementById("menu").style = "display: block";
+            counter++;
+        }
+    }  
 }
+
